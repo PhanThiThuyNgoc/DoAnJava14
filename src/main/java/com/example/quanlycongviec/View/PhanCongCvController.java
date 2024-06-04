95340f894a7e62b83846512d2783b695fb0a7cd5
PhanThiThuyNgoc <ngoccute080705@gmail.com>
2024-06-04 14:28:02 +0700

    giaodienquanly

---
+++ src/main/java/com/example/quanlycongviec/View/PhanCongCvController.java
@@ -0,0 +1,50 @@
+package com.example.quanlycongviec.View;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TableView;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PhanCongCvController implements Initializable {
+    @FXML
+    private Button sua;
+
+    @FXML
+    private TextArea ta_mota;
+
+    @FXML
+    private TableColumn<?, ?> table_Id;
+
+    @FXML
+    private TableColumn<?, ?> table_cv;
+
+    @FXML
+    private TextField tf_Ngaybd;
+
+    @FXML
+    private TextField tf_Ngayketthuc;
+
+    @FXML
+    private TextField tf_Nguoithuchien;
+
+    @FXML
+    private TextField tf_cv;
+
+    @FXML
+    private Button them;
+
+    @FXML
+    private Button xoa;
+
+    @FXML
+    private TableView<?> table_tong;
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
